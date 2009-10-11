04aacbbd77254b1f02c540c7e0cabd25542862cb
Kai Diefenbach <kai.diefenbach@iqpp.de>
2009-10-11 06:11:10 +0200

    Added metadata

---
+++ README.txt
@@ -0,0 +1,12 @@
+LFS - Lightning Fast Shop
+=========================
+
+LFS is an online shop based on Python, Django and jQuery.
+
+For more information please visit:
+
+   http://www.getlfs.com
+
+A demo shop can be tried here:
+
+   http://demo.getlfs.com
---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1.0'
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.txt')).read()
+
+setup(name='django-lfs',
+      version=version,
+      description='LFS - Lightning Fast Shop',
+      long_description=README,
+      classifiers=[
+          'Environment :: Web Environment',
+          'Framework :: Django',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+      ],
+      keywords='django e-commerce online-shop',
+      author='Kai Diefenbach',
+      author_email='kai.diefenbach@iqpp.de',
+      url='http://www.getlfs.com',
+      license='BSD',
+      packages=find_packages(exclude=['ez_setup']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'setuptools',
+      ],
+      )
