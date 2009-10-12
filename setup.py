13af6817d953699721c0d28c9885e1d738437503
Kai Diefenbach <kai.diefenbach@iqpp.de>
2009-10-12 09:28:23 +0200

    Added dependancies

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.1.0'
+version = '0.1.1'
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.txt')).read()
@@ -27,5 +27,8 @@ setup(name='django-lfs',
       zip_safe=False,
       install_requires=[
         'setuptools',
+        'django-lfstheme',
+        'django-portlets',
+        'django-review',
       ],
       )
