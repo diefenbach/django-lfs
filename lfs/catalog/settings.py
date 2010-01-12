b102d8aab17d90bb8178c3da0fb2778541a72625
zeidlerj <none@none>
2010-01-12 14:29:29 +0100

    adding nice description for templates

--- lfs/catalog/settings.py
+++ lfs/catalog/settings.py
@@ -93,14 +93,35 @@ IMAGES_PATH = "/media/lfs/icons" # Path to template preview images
 
 # template configuration for category display
 CATEGORY_TEMPLATES = (
-    (0,{"file":"%s/%s" % (CAT_PRODUCT_PATH ,"default.html"),"image":IMAGES_PATH+"/category_square.png"}),
-    (1,{"file":"%s/%s" % (CAT_PRODUCT_PATH ,"variant_1.html"),"image":IMAGES_PATH+"/category_horizontal.png"}),
-    (2,{"file":"%s/%s" % (CAT_CATEGORY_PATH ,"default.html"),"image":IMAGES_PATH+"/category_square.png"}),
-    (3,{"file":"%s/%s" % (CAT_CATEGORY_PATH ,"variant_1.html"),"image":IMAGES_PATH+"/category_horizontal.png"}),
+    (0,{"file":"%s/%s" % (CAT_PRODUCT_PATH ,"default.html"),
+        "image":IMAGES_PATH+"/category_square.png",
+        "name":_(u"Category with products"),
+        }),
+    (1,{"file":"%s/%s" % (CAT_PRODUCT_PATH ,"variant_1.html"),
+        "image":IMAGES_PATH+"/category_horizontal.png",
+        "name":_(u"Category with products horizontal"),        
+        }),
+    (2,{"file":"%s/%s" % (CAT_CATEGORY_PATH ,"default.html"),
+        "image":IMAGES_PATH+"/category_square.png",
+        "name":_(u"Category with subcategories"),		        
+        }),
+    (3,{"file":"%s/%s" % (CAT_CATEGORY_PATH ,"variant_1.html"),
+        "image":IMAGES_PATH+"/category_horizontal.png",
+        "name":_(u"Category with subcategories horizontal"),		
+        }),
 )
 # template configuration for product display    
 PRODUCT_TEMPLATES = (
-    (0,{"file":"%s/%s" % (PRODUCT_PATH ,"product_inline.html"),"image":IMAGES_PATH+"/product_default.png"}),
-    (1,{"file":"%s/%s" % (PRODUCT_PATH ,"product_inline_topprice.html"),"image":IMAGES_PATH+"/product_priceup.png"}),
-    (2,{"file":"%s/%s" % (PRODUCT_PATH ,"product_inline_topdescription.html"),"image":IMAGES_PATH+"/product_textup.png"}),
+    (0,{"file":"%s/%s" % (PRODUCT_PATH ,"product_inline.html"),
+        "image":IMAGES_PATH+"/product_default.png",
+        "name":_(u"Default template")
+        },),
+    (1,{"file":"%s/%s" % (PRODUCT_PATH ,"product_inline_topprice.html"),
+        "image":IMAGES_PATH+"/product_priceup.png",
+        "name":_(u"Price on top"),		
+        }),
+    (2,{"file":"%s/%s" % (PRODUCT_PATH ,"product_inline_topdescription.html"),
+        "image":IMAGES_PATH+"/product_textup.png",
+        "name":_(u"Description on top"),		
+        }),
 ) 
