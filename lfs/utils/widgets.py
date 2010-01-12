2b108051b40a749b17039b94bdd26606ba31103a
zeidlerj <none@none>
2010-01-12 09:03:45 +0100

    cleanup

--- lfs/utils/widgets.py
+++ lfs/utils/widgets.py
@@ -21,35 +21,18 @@ class SelectImage(Select):
         if value is None: value = ''
         self.image_id ="image_%s" % attrs["id"]
         final_attrs = self.build_attrs(attrs, name=name)
-        #onchange ="onChange='document.getElementById(\"%s\").src=this.options[this.selectedIndex].title;'" % (self.image_id) 
-        #alert(this.selectedIndex);
-        onchange ="onchange='alert(this.options[this.selectedIndex].title);'"         
-        output = [u'<table><tr> <td> <select%s%s>' % (flatatt(final_attrs),onchange) ]
-        options = self.render_options(choices, value)
-        if options:
-            output.append(options)
-        output.append('</select></td> <td> <img src="" id="%s"></td> </tr></table>' % self.image_id)               
-        #return mark_safe(u'\n'.join(output))
+        defaultimage = None
+        for id,keys in self.choices:
+            if str(id)==str(value):
+                defaultimage=keys["image"]
+        #try to pick first image as defaultimage
+        if defaultimage==None: 
+            if len(self.choices)>0: defaultimage = self.choices[0][1]["image"]
         return render_to_string("manage/widgets/selectimage.html",
-               {"selectimageid":self.image_id,"choices":self.choices,"currentvalue":value,"finalattrs":flatatt(final_attrs)})
-    def render_options(self, choices, selected_choices):
-        def render_option(option_value, option_label):
-            image= option_label['image']
-            file_url= option_label['file']                
-            option_value = force_unicode(option_value)
-            selected_html = (option_value in selected_choices) and u' selected="selected"' or ''            
-            return u'<option value="%s" title="%s"%s>%s</option>' % (
-                escape(option_value), escape(image),selected_html,
-                conditional_escape(force_unicode(file_url)))
-        # Normalize to strings.
-        selected_choices = set([force_unicode(v) for v in selected_choices])
-        output = []
-        for option_value, option_label in chain(self.choices, choices):
-            if isinstance(option_label, (list, tuple)):                
-                output.append(u'<optgroup label="%s">' % escape(force_unicode(option_value)))
-                for option in option_label:
-                    output.append(render_option(*option))
-                output.append(u'</optgroup>')
-            else:
-                output.append(render_option(option_value, option_label))
-        return u'\n'.join(output)
+               {"selectimageid":self.image_id,
+                "choices":self.choices,
+                "currentvalue":value,
+                "finalattrs":flatatt(final_attrs),
+                "imageurl":defaultimage,
+               })
+    
