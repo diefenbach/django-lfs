bd4470455e5490c0c09ce2576cf24e19a325a41b
mickt <michael.thornhill@gmail.com>
2010-01-22 14:42:13 +0000

    back out 103

--- lfs/addresses/ie/__init__.py
+++ lfs/addresses/ie/__init__.py
@@ -48,6 +48,7 @@ except ImportError, e:
         A Select widget that uses a list of Irish Counties as its choices.
         """
         def __init__(self, attrs=None):
+            from ie_counties import IE_COUNTY_CHOICES
             super(IECountySelect, self).__init__(attrs, choices=IE_COUNTY_CHOICES)
 
 
