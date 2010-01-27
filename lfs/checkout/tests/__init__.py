b3d5cd0d776b6518c60611cb3f651097998da91a
mickt <michael.thornhill@gmail.com>
2010-01-27 16:52:39 +0000

    move tests to a directory

---
+++ lfs/checkout/tests/__init__.py
@@ -0,0 +1,12 @@
+from test_checkout import *
+
+import os
+from windmill.authoring import djangotest 
+from django.conf import settings
+
+settings.RUN_WINDMILL_TESTS = True
+
+if settings.RUN_WINDMILL_TESTS:
+    class TestProjectWindmillTest(djangotest.WindmillDjangoUnitTest): 
+        test_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'windmilltests')
+        browser = 'firefox'
