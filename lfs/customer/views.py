56a43104707438bb13c682f76cb9e17e780ae1c4
Kai Diefenbach <kai.diefenbach@iqpp.de>
2010-02-07 08:59:20 +0100

    Added optional removing of address fields. Added removing of errors when address is changed via ajax.

--- lfs/customer/views.py
+++ lfs/customer/views.py
@@ -2,6 +2,7 @@
 from urlparse import urlparse
 
 # django imports
+from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.forms import PasswordChangeForm
@@ -35,7 +36,7 @@ from countries.models import Country
 def login(request, template_name="lfs/customer/login.html"):
     """Custom view to login or register/login a user.
 
-    The reason to use a custom login method are: 
+    The reason to use a custom login method are:
 
       * validate checkout type
       * integration of register and login form
@@ -43,7 +44,7 @@ def login(request, template_name="lfs/customer/login.html"):
     It uses Django's standard AuthenticationForm, though.
     """
     shop = lfs.core.utils.get_default_shop()
-    
+
     # If only anonymous checkout is allowed this view doesn't exists :)
     # if shop.checkout_type == CHECKOUT_TYPE_ANON:
     #     raise Http404()
@@ -55,34 +56,34 @@ def login(request, template_name="lfs/customer/login.html"):
 
     if request.POST.get("action") == "login":
         login_form = AuthenticationForm(data=request.POST)
-        
+
         if login_form.is_valid():
             redirect_to = request.POST.get("next")
             # Light security check -- make sure redirect_to isn't garbage.
             if not redirect_to or '//' in redirect_to or ' ' in redirect_to:
                 redirect_to = reverse("lfs_shop_view")
-            
+
             from django.contrib.auth import login
             login(request, login_form.get_user())
 
             return lfs.core.utils.set_message_cookie(
                 redirect_to, msg = _(u"You have been logged in."))
-        
+
     elif request.POST.get("action") == "register":
         register_form = RegisterForm(data=request.POST)
         if register_form.is_valid():
-                    
+
             email = register_form.data.get("email")
             password = register_form.data.get("password_1")
 
             # Create user
             user = User.objects.create_user(
                 username=email, email=email, password=password)
-            
+
             # Create customer
             customer = customer_utils.get_or_create_customer(request)
             customer.user = user
-        
+
             # Notify
             lfs.core.signals.customer_added.send(user)
 
@@ -90,16 +91,16 @@ def login(request, template_name="lfs/customer/login.html"):
             from django.contrib.auth import authenticate
             user = authenticate(username=email, password=password)
 
-            from django.contrib.auth import login                
+            from django.contrib.auth import login
             login(request, user)
-        
+
             redirect_to = request.POST.get("next")
             if not redirect_to or '//' in redirect_to or ' ' in redirect_to:
                 redirect_to = reverse("lfs_shop_view")
-            
+
             return lfs.core.utils.set_message_cookie(
                 redirect_to, msg = _(u"You have been registered and logged in."))
-    
+
     # Get next_url
     next_url = request.REQUEST.get("next")
     if next_url is None:
@@ -107,15 +108,15 @@ def login(request, template_name="lfs/customer/login.html"):
     if next_url is None:
         next_url =  reverse("lfs_shop_view")
 
-    # Get just the path of the url. See django.contrib.auth.views.login for more    
+    # Get just the path of the url. See django.contrib.auth.views.login for more
     next_url = urlparse(next_url)
     next_url = next_url[2]
-    
+
     try:
         login_form_errors = login_form.errors["__all__"]
     except KeyError:
         login_form_errors = None
-    
+
     return render_to_response(template_name, RequestContext(request, {
         "login_form" : login_form,
         "login_form_errors" : login_form_errors,
@@ -125,22 +126,22 @@ def login(request, template_name="lfs/customer/login.html"):
 
 def logout(request):
     """Custom method to logout a user.
-    
-    The reason to use a custom logout method is just to provide a login and a 
+
+    The reason to use a custom logout method is just to provide a login and a
     logoutmethod on one place.
     """
     from django.contrib.auth import logout
     logout(request)
-    
+
     return lfs.core.utils.set_message_cookie(reverse("lfs_shop_view"),
         msg = _(u"You have been logged out."))
-    
+
 @login_required
 def orders(request, template_name="lfs/customer/orders.html"):
     """Displays the orders of the current user
     """
     orders = Order.objects.filter(user=request.user)
-    
+
     return render_to_response(template_name, RequestContext(request, {
         "orders" : orders,
     }))
@@ -151,10 +152,10 @@ def order(request, id, template_name="lfs/customer/order.html"):
     """
     orders = Order.objects.filter(user=request.user)
     order = get_object_or_404(Order, pk=id, user=request.user)
-    
+
     return render_to_response(template_name, RequestContext(request, {
         "current_order" : order,
-        "orders" : orders,        
+        "orders" : orders,
     }))
 
 @login_required
@@ -173,7 +174,7 @@ def addresses(request, template_name="lfs/customer/addresses.html"):
     """
     customer = lfs.customer.utils.get_customer(request)
     shop = lfs.core.utils.get_default_shop()
-    
+
     if request.method == "POST":
         extra_data = {}
         prefixes = [INVOICE_PREFIX, SHIPPING_PREFIX]
@@ -279,6 +280,19 @@ def address_inline(request, prefix, form):
             if countries is not None:
                 address_form.fields["country"].choices = [(c.iso, c.name) for c in countries]
 
+    # Removes fields from address form if requested via settings.
+    for i in range(1, 6):
+        address_settings = getattr(settings, "POSTAL_ADDRESS_LINE%s" % i, None)
+        try:
+            if address_settings and address_settings[2] == False:
+                del address_form.fields["line%s" % i]
+        except IndexError:
+            pass
+
+    # if request via ajax don't display validity errors
+    if request.is_ajax():
+        address_form._errors = {}
+
     return render_to_string(template_name, RequestContext(request, {
         "address_form": address_form,
         "form": form,
@@ -322,7 +336,7 @@ def email(request, template_name="lfs/customer/email.html"):
             request.user.email = email_form.cleaned_data.get("email")
             request.user.save()
             return HttpResponseRedirect(reverse("lfs_my_email"))
-    else:        
+    else:
         email_form = EmailForm(initial={"email" : request.user.email})
 
     return render_to_response(template_name, RequestContext(request, {
@@ -339,8 +353,8 @@ def password(request, template_name="lfs/customer/password.html"):
             return HttpResponseRedirect(reverse("lfs_my_password"))
     else:
         form = PasswordChangeForm(request.user)
-        
+
     return render_to_response(template_name, RequestContext(request, {
         "form" : form
     }))
-    
+
