137f967baee29aadc35a6b1ba584b767b35d8d98
mickt <michael.thornhill@gmail.com>
2010-02-22 10:57:07 +0000

    remove windmill stuff

--- lfs/checkout/tests/__init__.py
+++ lfs/checkout/tests/__init__.py
@@ -1,13 +1,2 @@
 from test_checkout import *
 from test_addresses import *
-
-#import os
-#from windmill.authoring import djangotest
-#from django.conf import settings
-
-#settings.RUN_WINDMILL_TESTS = True
-
-#if settings.RUN_WINDMILL_TESTS:
-#    class TestProjectWindmillTest(djangotest.WindmillDjangoUnitTest):
-#        test_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'windmilltests')
-#        browser = 'firefox'
