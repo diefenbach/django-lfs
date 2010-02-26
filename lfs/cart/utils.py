03f946c63e7c3a201501d733de023c126c3236ec
Kai Diefenbach <kai.diefenbach@iqpp.de>
2010-02-26 08:45:54 +0100

    lfs.cart, bugfix: don't add shipping or payment price if the cart is empty

--- lfs/cart/utils.py
+++ lfs/cart/utils.py
@@ -47,13 +47,16 @@ def get_cart_costs(request, cart, total=False):
     cart_costs = cache.get(cache_key)
 
     if cart_costs is None:
+
+        items = cart.items()
+
         cart_price = 0
         cart_tax = 0
-        for item in cart.items():
+        for item in items:
             cart_price += item.get_price()
             cart_tax += item.get_tax()
 
-        if total:
+        if len(items) > 0 and total:
             # Shipping
             shipping_method = shipping_utils.get_selected_shipping_method(request)
             shipping_costs = shipping_utils.get_shipping_costs(request, shipping_method)
