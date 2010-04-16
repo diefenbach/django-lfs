abe613f11c354132c9d86a32f2e92cd595a7e2ac
Kai Diefenbach <kai.diefenbach@iqpp.de>
2010-04-16 12:54:05 +0200

    Fixed i18n for properties.

--- lfs/cart/views.py
+++ lfs/cart/views.py
@@ -231,7 +231,7 @@ def add_to_cart(request, product_id=None):
                 if property.is_input_field:
 
                     if (value < property.unit_min) or (value > property.unit_max):
-                        msg = "%s must be between %s and %s %s." % (property.name, property.unit_min, property.unit_max, property.unit)
+                        msg = _(u"%(name)s must be between %(min)s and %(max)s %(unit)s.") % {"name" : property.name, "min" : property.unit_min, "max" : property.unit_max, "unit" : property.unit }
                         return lfs.core.utils.set_message_cookie(
                             product.get_absolute_url(), msg)
 
@@ -245,7 +245,7 @@ def add_to_cart(request, product_id=None):
 
                     value = "%.2f" % value
                     if value not in steps:
-                        msg = _(u"Your entered value for %s (%s) is not in valid step width, which is %s." % (property.name, value, property.unit_step))
+                        msg = _(u"Your entered value for %(name)s (%(value)s) is not in valid step width, which is %(step)s.") % {"name": property.name, "value": value, "step" : property.unit_step }
                         return lfs.core.utils.set_message_cookie(
                             product.get_absolute_url(), msg)
 
