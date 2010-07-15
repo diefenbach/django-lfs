9eea23bb7a7b2e1a77e780624d2f6e06fc1f71a0
Kai Diefenbach <kai.diefenbach@iqpp.de>
2010-07-15 20:18:43 +0200

    Removed javascript which dynamically sets the height of the slots. This should rather be in javascript of lfstheme

--- lfs/static/js/lfs.js
+++ lfs/static/js/lfs.js
@@ -20,11 +20,7 @@ var update_checkout = function() {
 $(function() {
     // Delay plugin taken from ###############################################
     // http://ihatecode.blogspot.com/2008/04/jquery-time-delay-event-binding-plugin.html
-   
-   var hm = $("#middle-slot").height();
-   $(".left-slot").height(h)
-   $(".middle-slot").height(h)
-   
+
    $.fn.delay = function(options) {
         var timer;
         var delayImpl = function(eventObj) {
