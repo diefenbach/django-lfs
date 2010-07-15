a84c2fa4068a5bcc7cb1a81a631e6f0c950f54b5
Kai Diefenbach <kai.diefenbach@iqpp.de>
2010-07-15 16:06:15 +0200

    Added migration code for vouchers

---
+++ lfs/core/management/commands/lfs_migrate.py
@@ -0,0 +1,35 @@
+# django importsad
+from django.core.management.base import BaseCommand
+from django.db import models
+
+# lfs imports
+from lfs.voucher.models import Voucher
+
+# south imports
+from south.db import db
+
+class Command(BaseCommand):
+    args = ''
+    help = 'Migrations for LFS'
+
+    def handle(self, *args, **options):
+        """
+        """
+        # 0.5 -> trunk
+        db.add_column("voucher_voucher", "used_amount", models.PositiveSmallIntegerField(default=0))
+        db.add_column("voucher_voucher", "last_used_date", models.DateTimeField(blank=True, null=True))
+        db.add_column("voucher_voucher", "limit", models.PositiveSmallIntegerField(default=1))
+
+        for voucher in Voucher.objects.all():
+            voucher.limit = 1
+            voucher.save()
+
+        # This mus be done with execute because the old fields are not there
+        # anymore (and therefore can't access via attribute) after the user has
+        # upgraded to the latest version
+        db.execute("update voucher_voucher set used_amount = 1 where used = 1")
+        db.execute("update voucher_voucher set used_amount = 0 where used = 0")
+        db.execute("update voucher_voucher set last_used_date = used_date")
+
+        db.delete_column('voucher_voucher', 'used')
+        db.delete_column('voucher_voucher', 'used_date')
