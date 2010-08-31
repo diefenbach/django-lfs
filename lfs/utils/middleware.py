054dcd36bdca8c9aa26bb62ff4d1b38702d82108
Kai Diefenbach <kai.diefenbach@iqpp.de>
2010-08-31 07:29:19 +0200

    Provide own middleware in order to redirect urls with query string

--- lfs/utils/middleware.py
+++ lfs/utils/middleware.py
@@ -1,12 +1,41 @@
-import sys
-import tempfile
+# python imports
 import hotshot
 import hotshot.stats
-from django.conf import settings
+import sys
+import tempfile
+from urlparse import urlparse
 from cStringIO import StringIO
 
+# django imports
+from django import http
 from django.conf import settings
+from django.contrib.redirects.models import Redirect
 from django.http import HttpResponseServerError
+
+class RedirectFallbackMiddleware(object):
+    def process_response(self, request, response):
+        if response.status_code != 404:
+            return response # No need to check for a redirect for non-404 responses.        
+        path = urlparse(request.get_full_path()).path
+        
+        try:
+            r = Redirect.objects.get(site__id__exact=settings.SITE_ID, old_path=path)
+        except Redirect.DoesNotExist:
+            r = None
+        if r is None and settings.APPEND_SLASH:
+            # Try removing the trailing slash.
+            try:
+                r = Redirect.objects.get(site__id__exact=settings.SITE_ID,
+                    old_path=path[:path.rfind('/')]+path[path.rfind('/')+1:])
+            except Redirect.DoesNotExist:
+                pass
+        if r is not None:
+            if r.new_path == '':
+                return http.HttpResponseGone()
+            return http.HttpResponsePermanentRedirect(r.new_path)
+
+        # No redirect was found. Return the response.
+        return response
 
 class ProfileMiddleware(object):
     """
