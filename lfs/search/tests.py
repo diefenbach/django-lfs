a65cf44c182c7b0b3139e9936e1a32587c1db79d
mickt <michael.thornhill@gmail.com>
2010-11-12 09:16:29 +0000

    add a test case to test inactive searches

--- lfs/search/tests.py
+++ lfs/search/tests.py
@@ -15,6 +15,7 @@ class SearchTestCase(TestCase):
         """
         self.p1 = Product.objects.create(name="Product 1", slug="p1", price=9, active=True)
         self.p2 = Product.objects.create(name="Product 2", slug="p2", price=11, active=True)
+        self.p3 = Product.objects.create(name="Product 3", slug="p3", price=13, active=False)
 
     def test_search(self):
         """
@@ -28,3 +29,12 @@ class SearchTestCase(TestCase):
         # Must not be found
         response = self.client.get(url, {"q" : "Hurz"})
         self.failIf(response.content.find("Product 1") != -1)
+
+    def test_inactive_search(self):
+        """
+        """
+        url = reverse("lfs_search")
+
+        # Must not be found
+        response = self.client.get(url, {"q" : "Product"})
+        self.failIf(response.content.find("Product 3") != -1)
