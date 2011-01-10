33d5149feaac17025d987fa6825955327075afdf
mickt <michael.thornhill@gmail.com>
2011-01-10 14:04:12 +0000

    test for cache before committing to it

--- lfs/core/models.py
+++ lfs/core/models.py
@@ -184,7 +184,8 @@ class Shop(models.Model):
             return default_country
 
         default_country = self.default_country
-        cache.set(cache_key, default_country)
+        if cache:
+            cache.set(cache_key, default_country)
 
         return default_country
 
