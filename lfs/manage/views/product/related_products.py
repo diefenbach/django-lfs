6ba06c2e78b54a92644d77fc49697aa1713eb3d4
Kai Diefenbach <kai.diefenbach@iqpp.de>
2011-04-16 13:22:52 +0200

    Cleanups for related products management UI

--- lfs/manage/views/product/related_products.py
+++ lfs/manage/views/product/related_products.py
@@ -24,10 +24,19 @@ def manage_related_products(
     """
     product = Product.objects.get(pk=product_id)
     inline = manage_related_products_inline(request, product_id, as_string=True)
-    
+
+    # amount options
+    amount_options = []
+    for value in (10, 25, 50, 100):
+        amount_options.append({
+            "value" : value,
+            "selected" : value == request.session.get("accessories-amount")
+        })
+
     return render_to_string(template_name, RequestContext(request, {
         "product" : product,
         "related_products_inline" : inline,
+        "amount_options" : amount_options,
     }))
 
 @permission_required("core.manage_shop", login_url="/login/")
@@ -38,33 +47,39 @@ def manage_related_products_inline(
     product = Product.objects.get(pk=product_id)
     related_products = product.related_products.all()
     related_products_ids = [p.id for p in related_products]
-    
+
     r = request.REQUEST
     s = request.session
-    
-    # If we get the parameter ``keep-filters`` or ``page`` we take the 
+
+    # If we get the parameter ``keep-filters`` or ``page`` we take the
     # filters out of the request resp. session. The request takes precedence.
-    # The page parameter is given if the user clicks on the next/previous page 
+    # The page parameter is given if the user clicks on the next/previous page
     # links. The ``keep-filters`` parameters is given is the users adds/removes
-    # products. In this way we keeps the current filters when we needed to. If 
-    # the whole page is reloaded there is no ``keep-filters`` or ``page`` and 
+    # products. In this way we keeps the current filters when we needed to. If
+    # the whole page is reloaded there is no ``keep-filters`` or ``page`` and
     # all filters are reset as they should.
-    
+
     if r.get("keep-filters") or r.get("page"):
         page = r.get("page", s.get("related_products", 1))
         filter_ = r.get("filter", s.get("filter"))
         category_filter = r.get("related_products_category_filter",
                           s.get("related_products_category_filter"))
-    else:        
+    else:
         page = r.get("page", 1)
         filter_ = r.get("filter")
         category_filter = r.get("related_products_category_filter")
-    
+
     # The current filters are saved in any case for later use.
     s["related_products_page"] = page
     s["filter"] = filter_
     s["related_products_category_filter"] = category_filter
-    
+
+    try:
+        s["related-products-amount"] = int(r.get("related-products-amount",
+                                      s.get("related-products-amount")))
+    except TypeError:
+        s["related-products-amount"] = 25
+
     filters = Q()
     if filter_:
         filters &= (Q(name__icontains = filter_) | Q(sku__icontains = filter_))
@@ -78,18 +93,18 @@ def manage_related_products_inline(
             # First we collect all sub categories and using the `in` operator
             category = lfs_get_object_or_404(Category, pk=category_filter)
             categories = [category]
-            categories.extend(category.get_all_children())    
+            categories.extend(category.get_all_children())
             filters &= Q(categories__in = categories)
-    
+
     products = Product.objects.filter(filters).exclude(pk__in=related_products_ids).exclude(pk=product.pk)
-    paginator = Paginator(products, 20)
-    
+    paginator = Paginator(products, s["related-products-amount"])
+
     total = products.count()
     try:
         page = paginator.page(page)
     except EmptyPage:
         page = 0
-    
+
     result = render_to_string(template_name, RequestContext(request, {
         "product" : product,
         "related_products" : related_products,
@@ -98,8 +113,8 @@ def manage_related_products_inline(
         "paginator" : paginator,
         "filter" : filter_
     }))
-    
-    if as_string: 
+
+    if as_string:
         return result
     else:
         return HttpResponse(result)
@@ -117,51 +132,51 @@ def add_related_products(request, product_id):
     """Adds passed related products (by request body) to product with passed id.
     """
     parent_product = Product.objects.get(pk=product_id)
-    
+
     for temp_id in request.POST.keys():
 
         if temp_id.startswith("product") == False:
             continue
-        
+
         temp_id = temp_id.split("-")[1]
         parent_product.related_products.add(temp_id)
-        
+
         # This isn't necessary but it cleans the cache. See lfs.cache listeners
         # for more
         parent_product.save()
-    
-    inline = manage_related_products_inline(request, product_id, as_string=True)    
+
+    inline = manage_related_products_inline(request, product_id, as_string=True)
     result = simplejson.dumps({
         "html" : inline,
         "message" : _(u"Related products have been added.")
     }, cls=LazyEncoder);
-    
+
     return HttpResponse(result)
 
-@permission_required("core.manage_shop", login_url="/login/") 
+@permission_required("core.manage_shop", login_url="/login/")
 def remove_related_products(request, product_id):
     """Removes passed related products from product with passed id.
     """
     parent_product = Product.objects.get(pk=product_id)
-    
+
     for temp_id in request.POST.keys():
-        
+
         if temp_id.startswith("product") == False:
             continue
-        
+
         temp_id = temp_id.split("-")[1]
         parent_product.related_products.remove(temp_id)
-        
+
         # This isn't necessary but it cleans the cache. See lfs.cache listeners
         # for more
         parent_product.save()
-    
+
     inline = manage_related_products_inline(request, product_id, as_string=True)
     result = simplejson.dumps({
         "html" : inline,
         "message" : _(u"Related products have been removed.")
     }, cls=LazyEncoder);
-    
+
     return HttpResponse(result)
 
 @permission_required("core.manage_shop", login_url="/login/")
@@ -174,11 +189,11 @@ def update_related_products(request, product_id):
     else:
         product.active_related_products = False
     product.save()
-    
-    inline = manage_related_products_inline(request, product_id, as_string=True)    
+
+    inline = manage_related_products_inline(request, product_id, as_string=True)
     result = simplejson.dumps({
         "html" : inline,
         "message" : _(u"Related products have been updated.")
     }, cls=LazyEncoder);
-    
+
     return HttpResponse(result)
