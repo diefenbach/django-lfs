090e19d55b56173b647ed7405f07cca949ce60ae
Kai Diefenbach <kai.diefenbach@iqpp.de>
2011-04-16 12:48:16 +0200

    Improved products generator.

---
+++ lfs/utils/management/commands/lfs_generate_products.py
@@ -0,0 +1,11 @@
+# django imports
+from django.core.management.base import BaseCommand
+
+from lfs.utils import generator
+
+class Command(BaseCommand):
+    args = ''
+    help = 'Generates mock products for LFS'
+
+    def handle(self, *args, **options):
+        generator.products(20)
