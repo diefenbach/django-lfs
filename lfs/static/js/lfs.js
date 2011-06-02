b60a324b9e14205d61b200066057ff1638ccb81c
Kai Diefenbach <kai.diefenbach@iqpp.de>
2011-06-02 13:57:32 +0200

    Added javascript for CSRF protection for ajax requests.

--- lfs/static/js/lfs.js
+++ lfs/static/js/lfs.js
@@ -382,3 +382,25 @@ $(function() {
         });
     });
 })
+
+$(document).ajaxSend(function(event, xhr, settings) {
+    function sameOrigin(url) {
+        // url could be relative or scheme relative or absolute
+        var host = document.location.host; // host + port
+        var protocol = document.location.protocol;
+        var sr_origin = '//' + host;
+        var origin = protocol + sr_origin;
+        // Allow absolute or scheme relative URLs to same origin
+        return (url == origin || url.slice(0, origin.length + 1) == origin + '/') ||
+            (url == sr_origin || url.slice(0, sr_origin.length + 1) == sr_origin + '/') ||
+            // or any other URL that isn't scheme relative or absolute i.e relative.
+            !(/^(\/\/|http:|https:).*/.test(url));
+    }
+    function safeMethod(method) {
+        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+    }
+
+    if (!safeMethod(settings.type) && sameOrigin(settings.url)) {
+        xhr.setRequestHeader("X-CSRFToken", $.cookie("csrftoken"));
+    }
+});
