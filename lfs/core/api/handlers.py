5bffda62c4a5aab75dd3c3e6281a4ac368e9ecb8
mickt <michael.thornhill@gmail.com>
2011-06-09 21:32:29 +0200

    add api for categories

--- lfs/core/api/handlers.py
+++ lfs/core/api/handlers.py
@@ -1,6 +1,10 @@
 from piston.handler import BaseHandler
-from lfs.catalog.models import Product
+from lfs.catalog.models import Product, Category
 
 class ProductHandler(BaseHandler):
     allowed_methods = ('GET', )
     model = Product
+
+class CategoryHandler(BaseHandler):
+    allowed_methods = ('GET', )
+    model = Category
