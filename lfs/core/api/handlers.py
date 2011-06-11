86d2eb533ea855c710ecaf13070764f253f89c22
Kai Diefenbach <kai.diefenbach@iqpp.de>
2011-06-11 13:11:03 +0200

    PEP8 compliance

--- lfs/core/api/handlers.py
+++ lfs/core/api/handlers.py
@@ -1,9 +1,11 @@
 from piston.handler import BaseHandler
 from lfs.catalog.models import Product, Category
 
+
 class ProductHandler(BaseHandler):
     allowed_methods = ('GET', )
     model = Product
+
 
 class CategoryHandler(BaseHandler):
     allowed_methods = ('GET', )
