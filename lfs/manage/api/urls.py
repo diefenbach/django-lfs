9be82a4b83523916b959a7873d45481c1754a391
mickt <michael.thornhill@gmail.com>
2011-06-11 13:25:10 +0200

    protect the Category sort api with a login_required decorator

--- lfs/manage/api/urls.py
+++ lfs/manage/api/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls.defaults import patterns, url
+from django.contrib.auth.decorators import login_required
 from djangorestframework.resources import ModelResource
 from djangorestframework.views import ListOrCreateModelView, InstanceModelView
 from handlers import CategorySortView
 
 urlpatterns = patterns('',
-    url(r'^categories/sort/$', CategorySortView.as_view(), name="lfs-sort-categories"),
+    url(r'^categories/sort/$', login_required(CategorySortView.as_view()), name="lfs-sort-categories"),
 )
