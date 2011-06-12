e1165cb893e279c3194903a6cf3db317207449ed
Kai Diefenbach <kai.diefenbach@iqpp.de>
2011-06-12 08:02:25 +0200

    PEP8 compliance

--- lfs/tax/admin.py
+++ lfs/tax/admin.py
@@ -4,8 +4,9 @@ from django.contrib import admin
 # lfs imports
 from lfs.tax.models import Tax
 
+
 # Taxes
 class TaxAdmin(admin.ModelAdmin):
     """
-    """    
+    """
 admin.site.register(Tax, TaxAdmin)
--- lfs/tax/models.py
+++ lfs/tax/models.py
@@ -2,11 +2,20 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
+
 class Tax(models.Model):
-    """
+    """Represent a tax rate.
+
+    **Attributes**:
+
+    rate
+        The tax rate in percent.
+
+    description
+        The description of the tax rate.
     """
     rate = models.FloatField(_(u"Rate"), default=0)
     description = models.TextField(_(u"Description"), blank=True)
-    
+
     def __unicode__(self):
         return "%s%%" % self.rate
