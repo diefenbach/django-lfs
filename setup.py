a6b9079a143686e2a91c1c2d9eb428c7402e38ee
Kai Diefenbach <kai.diefenbach@iqpp.de>
2011-06-25 14:57:26 +0200

    Updated dependencies

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(name='django-lfs',
       dependency_links = ["http://pypi.iqpp.de/"],
       install_requires=[
         'setuptools',
-        'django-lfstheme == 0.5.0b6',
+        'django-lfstheme == 0.5.0b7',
         'django-portlets == 1.0',
         'django-pagination == 1.0.7',
         'django-reviews == 0.2.1',
