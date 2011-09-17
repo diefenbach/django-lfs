419c800d1e000ae4707d950ec6ea282c101dd8ff
mickt <michael.thornhill@gmail.com>
2011-09-17 16:12:14 +0100

    change tiny_mce.js location to new static location

--- lfs/static/js/lfs_tinymce.js
+++ lfs/static/js/lfs_tinymce.js
@@ -9,7 +9,7 @@ function addEditor(selector, hide_save) {
     // Theme options
     $(selector).tinymce({
         // Location of TinyMCE script
-        script_url : '/media/lfs/tiny_mce-3.4.2/tiny_mce.js',
+        script_url : '/static/tiny_mce-3.4.2/tiny_mce.js',
 
         // General options
         theme : "advanced",
