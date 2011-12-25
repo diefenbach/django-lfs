aa381e71091f57f2e9e92859598e5ecb30f7ed3c
Kai Diefenbach <kai.diefenbach@iqpp.de>
2011-12-25 13:58:44 +0100

    Added help to menu and started to point to the central docs.

--- lfs/core/context_processors.py
+++ lfs/core/context_processors.py
@@ -1,6 +1,7 @@
 # lfs imports
 from lfs.checkout.settings import CHECKOUT_TYPE_ANON
 from lfs.core.utils import get_default_shop
+from django.conf import settings
 
 
 def main(request):
@@ -11,4 +12,5 @@ def main(request):
     return {
         "SHOP": shop,
         "ANON_ONLY": shop.checkout_type == CHECKOUT_TYPE_ANON,
+        "LFS_DOCS": settings.LFS_DOCS,
     }
