d6a24f8eb437a946f47ae51c6f4b12ca51c24b69
Kai Diefenbach <kai.diefenbach@iqpp.de>
2012-01-08 18:16:33 +0100

    Bumped version to 0.6.4

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.6.3'
+version = '0.6.4'
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.txt')).read()
