63f69d696bd9a5c65bbb4bbb5a8fecff925b160d
Maciej Wiśniowski <pigletto@gmail.com>
2012-03-07 11:08:48 +0100

    handle immediate submit of Product Management form when autocomplete is used

--- lfs/static/js/lfs.manage.product.js
+++ lfs/static/js/lfs.manage.product.js
@@ -26,10 +26,19 @@ $(function() {
     };
 
     $('#id_manufacturer_autocomplete').autocomplete(MANUFACTURER_AUTOCOMPLETE_SETTINGS);
+    $('body').bind('form-save-start', function(evt){
+        if (evt.form_id == 'product-data-form'){
+            // ensure that manufacturer_id is cleared if autocomplete field is empty
+            if ($('#id_manufacturer_autocomplete').val() === ''){
+                $('#id_manufacturer').val('');
+            }
+        }
+    });
+
     // when form is reloaded we have to reattach autocomplete
     $('body').bind('form-save-end', function(evt){
         if (evt.form_id == 'product-data-form'){
             $('#id_manufacturer_autocomplete').autocomplete(MANUFACTURER_AUTOCOMPLETE_SETTINGS);
         }
-    })
+    });
 })
