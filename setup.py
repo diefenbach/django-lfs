4729b2e9881d9a3f8d8340f62f9b091c5c608b60
Kai Diefenbach <kai.diefenbach@iqpp.de>
2012-03-17 09:26:56 +0100

    Bumped version to 0.6.10.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.6.9'
+version = '0.6.10'
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.txt')).read()
@@ -32,7 +32,7 @@ setup(name='django-lfs',
         'django-pagination == 1.0.7',
         'django-paypal == 0.1.2',
         'django-portlets == 1.1',
-        'django-postal == 0.7.9',
+        'django-postal == 0.9',
         'django-reviews == 0.2.1',
         'django-tagging == 0.3.1',
         'djangorestframework == 0.2.3',
