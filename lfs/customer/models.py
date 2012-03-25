36b3aa2299fcb0a45c5a2713afd2acc6b8573151
Kai Diefenbach <kai.diefenbach@iqpp.de>
2012-03-25 19:02:45 +0200

    Bugfix: removed old street attribute out of Address' __unicode__ method; issue #172

--- lfs/customer/models.py
+++ lfs/customer/models.py
@@ -80,7 +80,7 @@ class Address(models.Model):
     email = models.EmailField(_("E-Mail"), blank=True, null=True, max_length=50)
 
     def __unicode__(self):
-        return "%s / %s" % (self.street, self.city)
+        return "%s / %s" % (self.line1, self.city)
 
 
 class BankAccount(models.Model):
