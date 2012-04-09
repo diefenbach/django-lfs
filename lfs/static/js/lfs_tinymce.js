3839f7058968ecc07dded014c334935a2655e628
Radim Novotny <novotny.radim@gmail.com>
2012-04-09 18:43:31 +0200

    Added verify_html option. It seems the 'clean' option is not taken into account

--- lfs/static/js/lfs_tinymce.js
+++ lfs/static/js/lfs_tinymce.js
@@ -29,6 +29,7 @@ function addEditor(selector, hide_save, height) {
         theme_advanced_toolbar_align : "left",
         save_onsavecallback : "save",
         relative_urls : false,
+        verify_html : false,
         cleanup : false,
         height : height,
         content_css : "/static/css/tinymce_styles.css",
