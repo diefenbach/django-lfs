ee0c70098c025a57fc01abd730ab58fd932b2d3e
Kai Diefenbach <kai.diefenbach@iqpp.de>
2012-05-12 16:10:31 +0200

    Fixed calculation of prices within GrossPriceCalculator when customer taxes are used.

--- lfs/gross_price/__init__.py
+++ lfs/gross_price/__init__.py
@@ -13,19 +13,19 @@ class GrossPriceCalculator(PriceCalculator):
         return self.get_price(with_properties) / self._calc_product_tax_rate()
 
     def get_price_gross(self, with_properties=True):
-        return self.get_price(with_properties)
+        return self.get_price_net(with_properties) * self._calc_customer_tax_rate()
 
     def get_standard_price_net(self, with_properties=True):
         return self.get_standard_price(with_properties) / self._calc_product_tax_rate()
 
     def get_standard_price_gross(self, with_properties=True):
-        return self.get_standard_price(with_properties)
+        return self.get_standard_price_net(with_properties) * self._calc_customer_tax_rate()
 
     def get_for_sale_price_net(self, with_properties=True):
         return self.get_for_sale_price(with_properties) / self._calc_product_tax_rate()
 
     def get_for_sale_price_gross(self, with_properties=True):
-        return self.get_for_sale_price(with_properties)
+        return self.get_for_sale_price_net(with_properties) * self._calc_customer_tax_rate()
 
     def price_includes_tax(self):
         return True
