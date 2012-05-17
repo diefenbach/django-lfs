d79dd599171bb8d3dd9fd0786d6509461da781d8
Kai Diefenbach <kai.diefenbach@iqpp.de>
2012-05-17 08:10:32 +0200

    Catch wrong floats in calculate_packing. Makes pass all tests again.

--- lfs/catalog/views.py
+++ lfs/catalog/views.py
@@ -71,7 +71,10 @@ def calculate_packing(request, id, quantity=None, with_properties=False, as_stri
     product = Product.objects.get(pk=id)
 
     if quantity is None:
-        quantity = float(request.POST.get("quantity"))
+        try:
+            quantity = float(request.POST.get("quantity"))
+        except (TypeError, ValueError):
+            quantity = 1
 
     packing_amount, packing_unit = product.get_packing_info()
 
