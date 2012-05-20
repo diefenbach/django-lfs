15134f556cecf72253d68300c7a771205d8c05a1
Kai Diefenbach <kai.diefenbach@iqpp.de>
2012-05-20 18:02:19 +0200

    Updated information for release 0.7.4

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.7.3'
+version = '0.7.4'
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.txt')).read()
