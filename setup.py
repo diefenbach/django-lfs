a3c0d14e017d7c81153d8596907ae96149480370
Kai Diefenbach <kai.diefenbach@iqpp.de>
2012-06-23 17:37:25 +0200

    Update version to 0.6.18

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.6.17'
+version = '0.6.18'
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.txt')).read()
