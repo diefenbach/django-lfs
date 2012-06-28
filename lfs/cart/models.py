ba411adf1a21fb8cb47d7cf4bddd83ab4bf018b9
Kai Diefenbach <kai.diefenbach@iqpp.de>
2012-06-28 11:12:11 +0200

    Fixed calculation of 'calculated price' for different locales.

--- lfs/cart/models.py
+++ lfs/cart/models.py
@@ -1,4 +1,5 @@
-
+# python imports
+import locale
 import re
 
 # django imports
@@ -269,7 +270,7 @@ class CartItem(models.Model):
                     po = PropertyOption.objects.get(pk=ppv.value)
                     value = po.price
                 else:
-                    value = ppv.value
+                    value = lfs.core.utils.atof(ppv.value)
                 pc = pc.replace(token, str(value))
             elif token.startswith("number"):
                 mo = re.match("number\((\d+)\)", token)
@@ -317,7 +318,7 @@ class CartItem(models.Model):
                 try:
                     value = format_string % float(cipv.value)
                 except ValueError:
-                    value = "%.2f" % float(cipv.value)
+                    value = locale.format("%.2f", lfs.core.utils.atof(cipv.value))
             else:
                 value = cipv.value
 
