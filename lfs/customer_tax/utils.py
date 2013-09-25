844bbf42548df85a8df920ba0111c4852c09e734
Pigletto <pigletto@gmail.com>
2013-09-25 17:37:51 +0200

    some caching

--- lfs/customer_tax/utils.py
+++ lfs/customer_tax/utils.py
@@ -6,11 +6,15 @@ from lfs.customer_tax.models import CustomerTax
 def get_customer_tax_rate(request, product):
     """Returns the specfic customer tax for the current customer and product.
     """
+    if hasattr(request, 'cached_customer_tax_rate'):
+        return getattr(request, 'cached_customer_tax_rate')
     customer_tax = get_first_valid(request, CustomerTax.objects.all(), product)
     if customer_tax:
-        return customer_tax.rate
+        taxrate = customer_tax.rate
     else:
-        return _calc_product_tax_rate(request, product)
+        taxrate = _calc_product_tax_rate(request, product)
+    setattr(request, 'cached_customer_tax_rate', taxrate)
+    return taxrate
 
 
 def _calc_product_tax_rate(request, product):
