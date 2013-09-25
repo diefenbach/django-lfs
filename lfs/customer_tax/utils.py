e366bdc80c26116e3d927ae944b25211fc103267
Maciej Wiśniowski <maciek@orbitvu.com>
2013-09-25 22:57:56 +0200

    fix for request = None

--- lfs/customer_tax/utils.py
+++ lfs/customer_tax/utils.py
@@ -6,14 +6,15 @@ from lfs.customer_tax.models import CustomerTax
 def get_customer_tax_rate(request, product):
     """Returns the specfic customer tax for the current customer and product.
     """
-    if hasattr(request, 'cached_customer_tax_rate'):
+    if request and hasattr(request, 'cached_customer_tax_rate'):
         return getattr(request, 'cached_customer_tax_rate')
     customer_tax = get_first_valid(request, CustomerTax.objects.all(), product)
     if customer_tax:
         taxrate = customer_tax.rate
     else:
         taxrate = _calc_product_tax_rate(request, product)
-    setattr(request, 'cached_customer_tax_rate', taxrate)
+    if request:
+        setattr(request, 'cached_customer_tax_rate', taxrate)
     return taxrate
 
 
