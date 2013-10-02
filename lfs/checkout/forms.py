401c78179909a1f88cc787fee1bba0795db44f68
Maciej Wiśniowski <pigletto@gmail.com>
2013-10-02 08:41:23 +0200

    allow for more than 9 payment methods

--- lfs/checkout/forms.py
+++ lfs/checkout/forms.py
@@ -6,7 +6,7 @@ from django.utils.translation import ugettext_lazy as _
 
 class OnePageCheckoutForm(forms.Form):
     requested_delivery_date = forms.DateField(label=_(u"Requested Delivery Date"), required=False)
-    payment_method = forms.CharField(required=False, max_length=1)
+    payment_method = forms.CharField(required=False, max_length=2)
     no_shipping = forms.BooleanField(label=_(u"Same as invoice"), initial=True, required=False)
     no_invoice = forms.BooleanField(label=_(u"Same as shipping address"), initial=True, required=False)
     message = forms.CharField(label=_(u"Your message to us"), widget=forms.Textarea(attrs={'cols': '80'}),
