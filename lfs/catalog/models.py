3072d3e57704a04a25bb915ab66709f809acdb51
Pigletto <pigletto@gmail.com>
2014-07-04 20:38:42 +0200

    fix issue with active_base_price/active_packing_price and checkbox input

--- lfs/catalog/models.py
+++ lfs/catalog/models.py
@@ -908,7 +908,8 @@ class Product(models.Model):
             else:
                 return self.active_base_price == CHOICES_YES
         else:
-            return self.active_base_price == CHOICES_YES
+            return self.active_base_price in (1, CHOICES_YES)  # we have to check for 1 as it's value set
+                                                               # by checkbox input
 
     def get_base_packing_price(self, request, with_properties=True):
         """
@@ -1821,7 +1822,7 @@ class Product(models.Model):
             else:
                 return self.active_packing_unit == CHOICES_YES
         else:
-            return self.active_packing_unit == CHOICES_YES
+            return self.active_packing_unit in (1, CHOICES_YES)
 
     def get_packing_info(self):
         """
--- lfs/core/widgets/checkbox.py
+++ lfs/core/widgets/checkbox.py
@@ -1,16 +1,7 @@
-from django.utils import six
 from django.forms.widgets import CheckboxInput
 
 
 class LFSCheckboxInput(CheckboxInput):
     def value_from_datadict(self, data, files, name):
-        if name not in data:
-            # A missing value means False because HTML form submission does not
-            # send results for unselected checkboxes.
-            return False
-        value = data.get(name)
-        # Translate true and false strings to boolean values.
-        values = {'true': True, 'false': False}
-        if isinstance(value, six.string_types):
-            value = values.get(value.lower(), value)
-        return value
+        res = super(LFSCheckboxInput, self).value_from_datadict(data, files, name)
+        return int(res)
