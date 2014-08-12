5109a4d4e3c2d2e7a9800f2ba199f23a6dd1bf66
Pigletto <pigletto@gmail.com>
2014-08-12 20:41:09 +0200

    fix urls for password reset

--- lfs/core/urls.py
+++ lfs/core/urls.py
@@ -98,8 +98,9 @@ urlpatterns += patterns('lfs.page.views',
 urlpatterns += patterns('django.contrib.auth.views',
     url(r'^password-reset/$', "password_reset", name="lfs_password_reset"),
     url(r'^password-reset-done/$', "password_reset_done", name="password_reset_done"),
-    url(r'^password-reset-confirm/(?P<uidb36>[-\w]*)/(?P<token>[-\w]*)$', "password_reset_confirm"),
-    url(r'^password-reset-complete/$', "password_reset_complete"),
+    url(r'^password-reset-confirm/(?P<uidb64>[-\w]*)/(?P<token>[-\w]*)$', "password_reset_confirm",
+        name="password_reset_confirm"),
+    url(r'^password-reset-complete/$', "password_reset_complete", name="password_reset_complete"),
 )
 
 # Search
