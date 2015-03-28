bc114db3d6c4eb507a61ab18e7809da4eb0b5646
Kai Diefenbach <kai.diefenbach@iqpp.de>
2015-03-28 10:33:05 +0100

    Moves the group id to the name.

--- lfs/utils/misc.py
+++ lfs/utils/misc.py
@@ -3,13 +3,13 @@ def parse_properties(request):
     property_id|option_id
     """
     properties = []
-    for property, option_id in request.POST.items():
-        if property.startswith("property"):
+    for prop, option_id in request.POST.items():
+        if prop.startswith("property"):
             try:
-                property_id = property.split("_")[1]
+                prop_group_id = prop.split("_")[1]
             except IndexError:
                 continue
             else:
-                property_group_id, option_id = option_id.split("|")
+                property_group_id, property_id = prop_group_id.split("|")
                 properties.append("%s|%s|%s" % (property_group_id, property_id, option_id))
     return properties
