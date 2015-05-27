627d7a24c6bfa2c79b4538e35e975a200e6fa10c
baffolobill <baffolobill@yandex.ru>
2015-05-27 15:55:17 +0300

    Replaced deprecated call of lfs.criteria.utils.is_valid

--- lfs/criteria/models.py
+++ lfs/criteria/models.py
@@ -641,12 +641,12 @@ class PaymentMethodCriterion(Criterion):
             return payment_method not in self.value.all()
         elif self.operator == self.IS_VALID:
             for pm in self.value.all():
-                if not lfs.criteria.utils.is_valid(self.request, pm, self.product):
+                if not pm.is_valid(self.request, self.product):
                     return False
             return True
         elif self.operator == self.IS_NOT_VALID:
             for pm in self.value.all():
-                if lfs.criteria.utils.is_valid(self.request, pm, self.product):
+                if pm.is_valid(self.request, self.product):
                     return False
             return True
         else:
@@ -709,12 +709,12 @@ class ShippingMethodCriterion(Criterion):
             return shipping_method not in self.value.all()
         elif self.operator == self.IS_VALID:
             for sm in self.value.all():
-                if not lfs.criteria.utils.is_valid(self.request, sm, self.product):
+                if not sm.is_valid(self.request, self.product):
                     return False
             return True
         elif self.operator == self.IS_NOT_VALID:
             for sm in self.value.all():
-                if lfs.criteria.utils.is_valid(self.request, sm, self.product):
+                if sm.is_valid(self.request, self.product):
                     return False
             return True
         else:
