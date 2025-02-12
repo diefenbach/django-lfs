f800fe3bc8988ef7aec8c2718844d68d8afeb986
Kai Diefenbach <kai.diefenbach@iqpp.de>
2025-02-12 09:08:53 +0100

    Remove dependencies from setup.py

--- setup.py
+++ setup.py
@@ -32,17 +32,5 @@ setup(
     dependency_links=["http://pypi.iqpp.de/"],
     install_requires=[
         "setuptools",
-        "Django >= 1.10, < 1.11",
-        "django-compressor == 2.1.1",
-        "django-localflavor == 1.4.1",
-        "django-paypal == 0.3.6",
-        "django-portlets >= 1.5, < 1.6",
-        "django-postal == 0.96",
-        "django-reviews >= 1.2, < 1.3",
-        "lfs-contact >= 1.3, < 1.4",
-        "lfs-order-numbers >= 1.2, < 1.3",
-        "lfs-paypal >= 1.4, < 1.5",
-        "lfs-theme >= 0.11, < 0.12",
-        "Pillow == 4.0",
     ],
 )
