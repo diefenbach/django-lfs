e9220a77790ca383cb457b645aaf40eddfd8ee9b
Kai Diefenbach <kai.diefenbach@iqpp.de>
2025-05-28 14:12:24 +0200

    Register StaticBlock model in the admin interface

--- lfs/catalog/admin.py
+++ lfs/catalog/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from lfs.catalog.models import Product
+from lfs.catalog.models import StaticBlock
 
 admin.site.register(Product)
+admin.site.register(StaticBlock)
