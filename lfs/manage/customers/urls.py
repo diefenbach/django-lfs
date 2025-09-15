94635457c55f68f3f2198fd02a3b74b044645aef
Kai Diefenbach <kai.diefenbach@iqpp.de>
2025-09-15 11:07:59 +0200

    refactor: standardize customer management URLs and templates
    - Renamed URL patterns for customer management to use a consistent prefix, improving clarity and organization.
    - Updated references in templates and tests to reflect the new URL names, ensuring all links point to the correct routes.

--- lfs/manage/customers/urls.py
+++ lfs/manage/customers/urls.py
@@ -23,31 +23,31 @@ urlpatterns = [
     path(
         "customers/apply-filters/",
         views.ApplyCustomerFiltersView.as_view(),
-        name="lfs_manage_apply_customer_filters",
+        name="lfs_apply_customer_filters",
     ),
     path(
         "customers/apply-filters-list/",
         views.ApplyCustomerFiltersView.as_view(),
-        name="lfs_manage_apply_customer_filters_list",
+        name="lfs_apply_customer_filters_list",
     ),
     path(
         "customer/<int:customer_id>/apply-predefined-filter/<str:filter_type>/",
         views.ApplyPredefinedCustomerFilterView.as_view(),
-        name="lfs_manage_apply_predefined_customer_filter",
+        name="lfs_apply_predefined_customer_filter",
     ),
     path(
         "customers/apply-predefined-filter-list/<str:filter_type>/",
         views.ApplyPredefinedCustomerFilterView.as_view(),
-        name="lfs_manage_apply_predefined_customer_filter_list",
+        name="lfs_apply_predefined_customer_filter_list",
     ),
     path(
         "customers/reset-filters/",
         views.ResetCustomerFiltersView.as_view(),
-        name="lfs_manage_reset_customer_filters",
+        name="lfs_reset_customer_filters",
     ),
     path(
         "customers/set-ordering/<str:ordering>/",
         views.SetCustomerOrderingView.as_view(),
-        name="lfs_manage_set_customer_ordering",
+        name="lfs_set_customer_ordering",
     ),
 ]
