01e9f26e3d628e79343c13012e4fe7c06d7d0961
Kai Diefenbach <kai.diefenbach@iqpp.de>
2025-09-16 11:36:35 +0200

    chore: add translation utility import in test image forms
    - Included the gettext_lazy import from django.utils.translation to facilitate translation in test image forms.
    - This addition prepares the test file for potential internationalization efforts, enhancing its adaptability for multilingual support.

--- lfs/manage/tests/images/test_image_forms.py
+++ lfs/manage/tests/images/test_image_forms.py
@@ -1,5 +1,6 @@
 import pytest
 from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 
 from lfs.catalog.models import Image
 from lfs.manage.images.forms import (
